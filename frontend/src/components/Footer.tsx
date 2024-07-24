6a702433e568f51db0a5a3adc97bdd9df9a6f41a
kartikbaghel05 <kratik.baghel2021@vitstudent.ac.in>
2024-07-24 23:39:39 +0530

    kartik

---
+++ frontend/src/components/Footer.tsx
@@ -0,0 +1,13 @@
+import * as React from "react";
+import "../styles/index.scss";
+
+const Footer: React.FC = () => {
+
+  return (
+    <footer>
+      <span>© 2024 Copyright: Pabak||Pratyush||Mubin||Kartik :Copyright 2024 ©</span>
+    </footer>
+  );
+};
+
+export default Footer;
